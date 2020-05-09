ea920100ebecdc3d103a4edfcca947c5296d1af9
Miguel Johnson <miguelbjohnson@gmail.com>
2020-05-09 11:27:12 -0400

    Test_Buzz: passed

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 def fizzbuzz(num):
-    if num % 3 ==0:
+    if num % 3 == 0:
         return 'Fizz'
+    elif num % 5 == 0:
+        return 'Buzz'
     return num
 
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -7,4 +7,6 @@ def test_number():
 
 def test_fizz():
     assert fizzbuzz(3) == 'Fizz'
-    
+
+def test_buzz():
+    assert fizzbuzz(5) == 'Buzz'
